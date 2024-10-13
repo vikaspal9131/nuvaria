9874378c0c0872fe02d8523b03aa8f87270c3c27
vikaspal9131 <vikaspal968562@gmail.com>
2024-10-13 20:56:24 +0530

    upload all the file

---
+++ script/dashbord.js
@@ -0,0 +1,18 @@
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+		{
+			headers: {
+				Authorization: "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+query({"inputs": "Astronaut riding a horse"}).then((response) => {
+	// Use image
+});
