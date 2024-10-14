a03e5307262e257a835880a0191de1fd67e3c60e
vikaspal9131 <vikaspal968562@gmail.com>
2024-10-14 18:24:13 +0530

    working on dashbord page

--- script/dashbord.js
+++ script/dashbord.js
@@ -1,18 +1,30 @@
-async function query(data) {
+const token = "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"
+const inputTxt = document.getElementById("input")
+const image = document.getElementById("image")
+const btn = document.getElementById("btn")
+
+
+
+
+async function query() {
 	const response = await fetch(
 		"https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
 		{
 			headers: {
-				Authorization: "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+				Authorization: `Bearer ${token}` ,
 				"Content-Type": "application/json",
 			},
 			method: "POST",
-			body: JSON.stringify(data),
+			body: JSON.stringify({"inputs": inputTxt.value}),
 		}
 	);
 	const result = await response.blob();
 	return result;
 }
-query({"inputs": "Astronaut riding a horse"}).then((response) => {
-	// Use image
-});
+
+btn.addEventListener('click' , async function () {
+	query().then((response) => {
+		const objectURL = URL.createObjectURL(response)
+		image.src = objectURL
+	});
+})
