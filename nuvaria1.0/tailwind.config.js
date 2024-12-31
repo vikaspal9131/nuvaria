96965ec1260df833fcf1ed57d5c1fe44426e3dfc
vikaspal9131 <vikaspal968562@gmail.com>
2025-01-01 00:51:31 +0530

    add new update code of tailwind css

---
+++ nuvaria1.0/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      spacing: {
+        'wide-word': '10000px', // Adjust as needed
+      },
+    },
+  },
+  plugins: [],
+}
+
