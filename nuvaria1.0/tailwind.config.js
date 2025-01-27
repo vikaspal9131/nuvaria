1c1bfcccab2c3faafd32de2154ddfaa9d82d64f3
vikaspal9131 <vikaspal968562@gmail.com>
2025-01-27 11:56:24 +0530

    fix layout of dashboard 🐉

--- nuvaria1.0/tailwind.config.js
+++ nuvaria1.0/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       spacing: {
         'wide-word': '10000px', // Adjust as needed
       },
+      colors: {
+        'custom-color': '#1F1F1F', // Replace with your color name and value
+      },
     },
   },
   plugins: [],
