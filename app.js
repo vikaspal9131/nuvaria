c1fd4c4613f4fdd29417dfec1c56bacfd2bd648e
vikaspal9131 <vikaspal968562@gmail.com>
2025-02-05 00:13:35 +0530

    update 🍉

--- app.js
+++ app.js
@@ -3,28 +3,27 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the 'public' folder
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve index.html
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Route to serve login.html
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// Route to serve dashboard.html
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
-// Set the port for the app (using the environment variable from Render or 3000 locally)
+
 const PORT = process.env.PORT || 3000;
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
